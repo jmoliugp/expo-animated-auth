ac500819e0d3b3c2d6a76d9d2e4a99e0c0f4c8ae
Solo <juanoliugomezplatero@gmail.com>
2024-02-13 14:39:42 -0300

    snack bar integration

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -8,21 +8,21 @@ import Animated, {
 
 interface Props {
   message: string
-  isVisible: boolean
+  triggerKey: number
 }
 
-export const SnackBar: React.FC<Props> = ({ message, isVisible }) => {
+export const SnackBar: React.FC<Props> = ({ message, triggerKey }) => {
   const opacity = useSharedValue(0)
 
   useEffect(() => {
-    if (isVisible) {
-      opacity.value = withTiming(1, { duration: 500 })
+    opacity.value = withTiming(1, { duration: 500 })
 
-      setTimeout(() => {
-        opacity.value = withTiming(0, { duration: 500 })
-      }, 3000)
-    }
-  }, [isVisible, opacity])
+    const timeoutId = setTimeout(() => {
+      opacity.value = withTiming(0, { duration: 500 })
+    }, 3000)
+
+    return () => clearTimeout(timeoutId)
+  }, [triggerKey])
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
@@ -32,7 +32,7 @@ export const SnackBar: React.FC<Props> = ({ message, isVisible }) => {
 
   return (
     <Animated.View
-      className="absolute left-0 right-0 items-center justify-center p-3 bg-black bottom-12"
+      className="absolute left-0 right-0 z-10 items-center justify-center p-3 bg-black bottom-16"
       style={animatedStyle}>
       <Text className="text-white">{message}</Text>
     </Animated.View>
